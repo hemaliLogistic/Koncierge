41c3f31679f0d3ba1bf532a50f48287a82c232ad
hemali logistic <hemali@logisticinfotech.co.in>
2024-07-19 19:10:35 +0530

    url change

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { HOST_API } from "../../predict";
 
 const axiosInstance = axios.create({
-  baseURL: HOST_API || "http://3.232.124.157:1337/api/v1",
+  baseURL: "http://3.232.124.157:1337/api/v1",
 });
 
 export default axiosInstance;

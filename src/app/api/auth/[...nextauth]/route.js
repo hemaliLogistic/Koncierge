a36c60bef3f63da9a5bab7b54d998b209c029764
hemali logistic <hemali@logisticinfotech.co.in>
2024-07-22 12:27:29 +0530

    fb change

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -23,36 +23,9 @@ const handler = NextAuth({
       clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
     }),
-    // FacebookProvider({
-    //   clientId: FACEBOOK_ID,
-    //   clientSecret: FACEBOOK_SECRET,
-    // }),
     FacebookProvider({
-      idToken: true,
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      authorization: {
-        url: "https://www.facebook.com/v11.0/dialog/oauth",
-        params: {
-          client_id: process.env.FACEBOOK_CLIENT_ID,
-          scope: "openid email",
-          response_type: "code",
-        },
-      },
-      token: {
-        url: "https://graph.facebook.com/oauth/access_token",
-        async request(context) {
-          const url =
-            `https://graph.facebook.com/oauth/access_token` +
-            `?code=${context.params.code}` +
-            `&client_id=${context.provider.clientId}` +
-            `&redirect_uri=${context.provider.callbackUrl}` +
-            `&client_secret=${context.provider.clientSecret}`;
-          const response = await fetch(url);
-          const tokens = await response.json();
-          return { tokens };
-        },
-      },
+      clientId: FACEBOOK_ID,
+      clientSecret: FACEBOOK_SECRET,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,

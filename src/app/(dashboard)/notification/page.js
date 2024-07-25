54442e516cd2efcd94f197b10f64b711594ffef5
hemali logistic <hemali@logisticinfotech.co.in>
2024-07-25 17:45:19 +0530

    notification page change

--- src/app/(dashboard)/notification/page.js
+++ src/app/(dashboard)/notification/page.js
@@ -2,153 +2,174 @@
 
 import React from "react";
 import "../../../components/NavBar/global.css";
-import {
-    Modal,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Button,
-    useDisclosure,
-  } from "@nextui-org/react";
-import "../notification/global.css"
+
+import "../notification/global.css";
 
 const UserDashBoard = () => {
-    return (
-        <div className="main-container">
-            <div><p className="notification-header">Notifications</p></div>
-            <div className="horizontal-line-themecolor"></div>
-            <div className="notification-box">
-                <div className="notification-detail-container">
-                    <div className="left-section">
-                        <div className="flex">
-                        <img src="/images/userProfile.png" className="user-profile-image"/>
-                        <p className="user-name">Name Here</p>
-                        </div>
-
-                        <div className="user-message">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-                    </div>
-
-                    <div className="right-section">
-                        <div className="btn-container">
-                            <p className="btn-text">Accept</p>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="horizontal-line-graycolor"></div>
-
-                <div className="notification-detail-container">
-                    <div className="left-section">
-                        <div className="flex">
-                        <img src="/images/userProfile.png" className="user-profile-image"/>
-                        <p className="user-name">Name Here</p>
-                        </div>
-
-                        <div className="user-message">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-                    </div>
-
-                    <div className="right-section">
-                        <div className="btn-container">
-                            <p className="btn-text">Accept</p>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="horizontal-line-graycolor"></div>
-
-
-                <div className="notification-detail-container">
-                    <div className="left-section">
-                        <div className="flex">
-                        <img src="/images/userProfile.png" className="user-profile-image"/>
-                        <p className="user-name">Name Here</p>
-                        </div>
-
-                        <div className="user-message">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-                    </div>
-
-                    <div className="right-section">
-                        <div className="btn-container">
-                            <p className="btn-text">Accept</p>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="horizontal-line-graycolor"></div>
-
-
-                <div className="notification-detail-container">
-                    <div className="left-section">
-                        <div className="flex">
-                        <img src="/images/userProfile.png" className="user-profile-image"/>
-                        <p className="user-name">Name Here</p>
-                        </div>
-
-                        <div className="user-message">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-                    </div>
-
-                    <div className="right-section">
-                        <div className="btn-container">
-                            <p className="btn-text">Accept</p>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="horizontal-line-graycolor"></div>
-
-
-                <div className="notification-detail-container">
-                    <div className="left-section">
-                        <div className="flex">
-                        <img src="/images/userProfile.png" className="user-profile-image"/>
-                        <p className="user-name">Name Here</p>
-                        </div>
-
-                        <div className="user-message">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-                    </div>
-
-                    <div className="right-section">
-                        <div className="btn-container">
-                            <p className="btn-text">Accept</p>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="horizontal-line-graycolor"></div>
-
-                <div className="notification-detail-container">
-                    <div className="left-section">
-                        <div className="flex">
-                        <img src="/images/userProfile.png" className="user-profile-image"/>
-                        <p className="user-name">Name Here</p>
-                        </div>
-
-                        <div className="user-message">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-                    </div>
-
-                    <div className="right-section">
-                        <div className="btn-container">
-                            <p className="btn-text">Accept</p>
-                        </div>
-                    </div>
-                    
-                </div>
+  return (
+    <div className='main-container'>
+      <div>
+        <p className='notification-header'>Notifications</p>
+      </div>
+      <div className='horizontal-line-themecolor'></div>
+      <div className='notification-box'>
+        <div className='notification-detail-container'>
+          <div className='left-section'>
+            <div className='flex'>
+              <img
+                src='/images/userProfile.png'
+                className='user-profile-image'
+              />
+              <p className='user-name'>Name Here</p>
             </div>
+
+            <div className='user-message'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+
+          <div className='right-section'>
+            <div className='btn-container'>
+              <p className='btn-text'>Accept</p>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className='horizontal-line-graycolor'></div>
+
+        <div className='notification-detail-container'>
+          <div className='left-section'>
+            <div className='flex'>
+              <img
+                src='/images/userProfile.png'
+                className='user-profile-image'
+              />
+              <p className='user-name'>Name Here</p>
+            </div>
+
+            <div className='user-message'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+
+          <div className='right-section'>
+            <div className='btn-container'>
+              <p className='btn-text'>Accept</p>
+            </div>
+          </div>
+        </div>
+        <div className='horizontal-line-graycolor'></div>
+
+        <div className='notification-detail-container'>
+          <div className='left-section'>
+            <div className='flex'>
+              <img
+                src='/images/userProfile.png'
+                className='user-profile-image'
+              />
+              <p className='user-name'>Name Here</p>
+            </div>
+
+            <div className='user-message'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+
+          <div className='right-section'>
+            <div className='btn-container'>
+              <p className='btn-text'>Accept</p>
+            </div>
+          </div>
+        </div>
+        <div className='horizontal-line-graycolor'></div>
+
+        <div className='notification-detail-container'>
+          <div className='left-section'>
+            <div className='flex'>
+              <img
+                src='/images/userProfile.png'
+                className='user-profile-image'
+              />
+              <p className='user-name'>Name Here</p>
+            </div>
+
+            <div className='user-message'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+
+          <div className='right-section'>
+            <div className='btn-container'>
+              <p className='btn-text'>Accept</p>
+            </div>
+          </div>
+        </div>
+        <div className='horizontal-line-graycolor'></div>
+
+        <div className='notification-detail-container'>
+          <div className='left-section'>
+            <div className='flex'>
+              <img
+                src='/images/userProfile.png'
+                className='user-profile-image'
+              />
+              <p className='user-name'>Name Here</p>
+            </div>
+
+            <div className='user-message'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+
+          <div className='right-section'>
+            <div className='btn-container'>
+              <p className='btn-text'>Accept</p>
+            </div>
+          </div>
+        </div>
+        <div className='horizontal-line-graycolor'></div>
+
+        <div className='notification-detail-container'>
+          <div className='left-section'>
+            <div className='flex'>
+              <img
+                src='/images/userProfile.png'
+                className='user-profile-image'
+              />
+              <p className='user-name'>Name Here</p>
+            </div>
+
+            <div className='user-message'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+
+          <div className='right-section'>
+            <div className='btn-container'>
+              <p className='btn-text'>Accept</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UserDashBoard;
